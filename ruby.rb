55d206624ce0ae11d9448b49ea1202901e8c172b
BlueCodeThree <carliemhamilton@gmail.com>
2019-03-30 19:49:33 +1000

    added ruby commands

--- ruby.rb
+++ ruby.rb
@@ -1,8 +1,75 @@
+ARRAY_ANSWER = %q(
+    Create an array:
+`array = Array.new`
+
+Create an array with three items:
+`array = Array.new(3, "Jason")`
+
+Create an empty array with bracket notation:
+`array = []`
+
+Create an array using brackets with three items:
+`array = ["milk", "eggs", "bread"]`
+
+Create an array using the %w notation which contains three strings:
+`array = %w(milk eggs bread)`
+
+Create an array using the %W notation which contains three strings and one is interpolated:
+```item = “milk”
+array = %W(#{item} eggs bread)```
+
+How to cycle through arrays - for loop
+```numbers = [1, 2, 3, 4, 5]
+
+for number in numbers 
+    puts " there are #{number} bottles!"
+end
+```
+
+Cycle through array - each loop
+```groceries = ["bananas", "apples", "bread", "milk"]
+
+groceries.each do |food|
+    puts "Get some #{food}"
+end
+``` 
+
+Items can be added to the end of an array by using either push or <<
+```arr = [1, 2, 3, 4]
+arr.push(5) #=> [1, 2, 3, 4, 5]
+arr << 6    #=> [1, 2, 3, 4, 5, 6]```
+)
+
 CONVERT_ANSWER = %q(
     Converting strings, integers and floats etc
 ```variable_name.to_s # to string
 variable_name.to_f # to float
 variable_name.to_i # to integer
+```
+)
+
+EXCEPTION_ANSWER = %q(
+**Exceptions**
+```
+begin
+    # code goes here
+rescue
+    # code here what happens if there is an error
+    puts "You can't do that!"
+    exit
+end
+```
+
+another way using "raise"
+```def check_age(age)
+    raise ArgumentError, "Enter Positive Number" unless age > 0
+end
+
+begin
+    check_age(-1)
+rescue ArgumentError
+    puts "That is an impossible age"
+end
 ```
 )
 
@@ -59,6 +126,60 @@ else
 LENGTH_ANSWER = %q(
 Getting the length of a string in ruby: 
     `variable_name.length`
+)
+
+LOOP_ANSWER = %q(
+**Loops/Conditionals for Ruby**
+**next** goes back to the top of the loop
+unless means "not"
+break ends the loop.
+```loop do 
+    x += 1
+    next unless (x % 2) == 0
+    puts x
+    break if x >= 10
+end
+```
+So in the above example, the loop skips over the number if it is odd, and puts the number if it is not odd, and breaks if x is bigger than or equal to 10.
+
+**While**
+```while y <= 10
+    y += 1
+    puts y
+end```
+
+**Until**
+```until a >= 0
+    a+= 1
+    next unless (a % 2) == 0
+    puts a
+end
+```
+How to cycle through arrays - for loop
+```numbers = [1, 2, 3, 4, 5]
+
+for number in numbers 
+    puts " there are #{number} bottles!"
+end
+```
+
+Cycle through array each 
+```groceries = ["bananas", "apples", "bread", "milk"]
+
+groceries.each do |food|
+    puts "Get some #{food}"
+end
+``` 
+
+cycling through numbers - a range:
+```(1..10).each do |i|
+    puts "number #{i}"
+end
+
+for year in 2000...2015
+    puts year
+end
+    ```
 )
 
 PRINT_ANSWER = %q(
--- tutorbot.rb
+++ tutorbot.rb
@@ -12,12 +12,32 @@ prefix = "!"
 bot = Discordrb::Bot.new token: TOKEN, client_id: CLIENT
 
 ## RUBY -- hopefully in alphabetical order
+bot.message(with_text: prefix + 'rb array') do |event|
+    event.respond ARRAY_ANSWER
+end
+
 bot.message(with_text: prefix + 'rb case') do |event|
     event.respond IF_ANSWER
 end
 
+bot.message(with_text: prefix + 'rb conditional') do |event|
+    event.respond LOOP_ANSWER
+end
+
 bot.message(with_text: prefix + 'rb convert') do |event|
     event.respond CONVERT_ANSWER
+end
+
+bot.message(with_text: prefix + 'rb each') do |event|
+    event.respond LOOP_ANSWER
+end
+
+bot.message(with_text: prefix + 'rb exceptions') do |event|
+    event.respond EXCEPTION_ANSWER
+end
+
+bot.message(with_text: prefix + 'rb for') do |event|
+    event.respond LOOP_ANSWER
 end
 
 bot.message(with_text: prefix + 'rb function') do |event|
@@ -40,6 +60,14 @@ bot.message(with_text: prefix + 'rb length') do |event|
     event.respond LENGTH_ANSWER
 end
 
+bot.message(with_text: prefix + 'rb loop') do |event|
+    event.respond LOOP_ANSWER
+end
+
+bot.message(with_text: prefix + 'rb next') do |event|
+    event.respond LOOP_ANSWER
+end
+
 bot.message(with_text: prefix + 'rb print') do |event|
   event.respond PRINT_ANSWER
 end
@@ -47,6 +75,10 @@ end
 bot.message(with_text: prefix + 'rb puts') do |event|
     event.respond PRINT_ANSWER
   end
+
+  bot.message(with_text: prefix + 'rb range') do |event|
+    event.respond LOOP_ANSWER
+end
 
 bot.message(with_text: prefix + 'rb string') do |event|
 event.respond STRING_ANSWER
@@ -58,6 +90,14 @@ end
 
 bot.message(with_text: prefix + 'rb unless') do |event|
 event.respond IF_ANSWER
+end
+
+bot.message(with_text: prefix + 'rb until') do |event|
+    event.respond LOOP_ANSWER
+end
+
+bot.message(with_text: prefix + 'rb while') do |event|
+    event.respond LOOP_ANSWER
 end
  
 # Wake up the bot!
