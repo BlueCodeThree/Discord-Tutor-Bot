f41bb2a27d5e2839387121e8c90e52afc0fb0d27
BlueCodeThree <carliemhamilton@gmail.com>
2019-03-30 18:20:55 +1000

    adding more ruby commands

--- ruby.rb
+++ ruby.rb
@@ -1,6 +1,64 @@
+CONVERT_ANSWER = %q(
+    Converting strings, integers and floats etc
+```variable_name.to_s # to string
+variable_name.to_f # to float
+variable_name.to_i # to integer
+```
+)
+
+FUNCTION_ANSWER = %q(
+    **Function/ Method for ruby**
+Note that there is no ":" etc. 
+    ```def functionName(stuff, here)
+        # things for the function go here
+    end
+        ```
+No need for the brackets if you don't have any parameters. 
+)
+
+GETS_ANSWER = %q(
+    asks for user input. The "chomp" removes an enter key at the end of the input
+    `gets.chomp`
+
+    string on more methods!
+    `gets.chomp.to_i`
+)
+
+IF_ANSWER = %q(
+    **if conditionals**. Notice no ":" etc
+```if true
+    # output here
+elsif condition
+    # output here
+else conditions
+    # output here
+end```
+        
+Another way to do if statements:
+`puts "you're young" if age < 30`
+
+If age is bigger than or equal to 30, it'll puts "Old", else puts "Young"
+`puts (age >= 30) ? "Old" : "Young"`
+
+**'unless'** is confusing and means the opposite of 'if', so it runs if the parameters are false.
+```unless variable >= 50 # variable is NOT bigger than or equal to 50
+    # output here
+end```
+
+**Case**
+```fruit = "pear"
+case fruit
+when "pear"
+    # do this
+when "apple"
+    # do this
+else 
+    # this ```
+)
+
 LENGTH_ANSWER = %q(
 Getting the length of a string in ruby: 
-    `variableName.length`
+    `variable_name.length`
 )
 
 PRINT_ANSWER = %q(
@@ -26,6 +84,25 @@ Some things you can do with puts:
 You can put sums inside the string
 `puts "Here is a string with math #{3 + 5}"`
  )  
+
+STRING_ANSWER = %q(
+    **Stuff about strings in ruby**
+`variable_name.to_s  # convert to string`
+
+`"string here".length # get the length`
+
+interpolated variable
+`"This is a string with a #{variable}"`
+
+and combining strings
+`full_name = "#{first_name} #{middle_name} #{last_name}"`
+
+```puts "Vowels: " + full_name.count("aeiou").to_s # counts the number of vowels
+puts "Consonants: " + full_name.count("^aeiou").to_s```
+
+```full_name.include?("Justine") # will say either true or false
+full_name.index("Justine").to_s # the number index where this text occurs```
+)
 
 TIME_ANSWER = %q(
 gets the time right now
--- tutorbot.rb
+++ tutorbot.rb
@@ -12,6 +12,30 @@ prefix = "!"
 bot = Discordrb::Bot.new token: TOKEN, client_id: CLIENT
 
 ## RUBY -- hopefully in alphabetical order
+bot.message(with_text: prefix + 'rb case') do |event|
+    event.respond IF_ANSWER
+end
+
+bot.message(with_text: prefix + 'rb convert') do |event|
+    event.respond CONVERT_ANSWER
+end
+
+bot.message(with_text: prefix + 'rb function') do |event|
+    event.respond FUNCTION_ANSWER
+end
+
+bot.message(with_text: prefix + 'rb gets') do |event|
+    event.respond GETS_ANSWER
+end
+
+bot.message(with_text: prefix + 'rb if') do |event|
+    event.respond IF_ANSWER
+end
+
+bot.message(with_text: prefix + 'rb input') do |event|
+    event.respond GETS_ANSWER
+end
+
 bot.message(with_text: prefix + 'rb length') do |event|
     event.respond LENGTH_ANSWER
 end
@@ -24,8 +48,16 @@ bot.message(with_text: prefix + 'rb puts') do |event|
     event.respond PRINT_ANSWER
   end
 
+bot.message(with_text: prefix + 'rb string') do |event|
+event.respond STRING_ANSWER
+end
+
 bot.message(with_text: prefix + 'rb time') do |event|
 event.respond TIME_ANSWER
+end
+
+bot.message(with_text: prefix + 'rb unless') do |event|
+event.respond IF_ANSWER
 end
  
 # Wake up the bot!
